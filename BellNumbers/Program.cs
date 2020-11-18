c7ee2b1ff490a39c3d13fda57badde6179094455
xgrois <67983850+xgrois@users.noreply.github.com>
2020-11-18 19:05:29 +0100

    Add Bell numbers project.

---
+++ BellNumbers/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace BellNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("::: Bell numbers :::");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"Bell({i}) --> {BellDP(i)}");
+            }
+            
+
+        }
+
+        /// <summary>
+        /// Bell number using bottom-up Dynamic Programming.
+        /// It is based on the recurrence relation using Stirling numbers
+        /// B(n) = sum_{k = 0}^{n} S(n,k)
+        /// </summary>
+        /// <param name="n">nth number in Bell sequence</param>
+        /// <returns></returns>
+        static BigInteger BellDP(int n)
+        {
+            List<BigInteger> prevRow = new List<BigInteger>();
+            List<BigInteger> currRow = new List<BigInteger>();
+
+            if (n == 0 || n == 1) return 1;
+
+            // Progressively compute Stirling rows
+            // Only two lists are needed (prev row, current row)
+            // Update them as progression is neeeded
+            int k = 0;
+            BigInteger Snk = 0;
+            prevRow.Add(0); prevRow.Add(1);
+            for (int i = 2; i <= n; i++)
+            {
+                currRow.Add(0);
+                for (k = 1; k < i; k++)
+                {
+                    Snk = prevRow[k - 1] + k * prevRow[k];
+                    currRow.Add(Snk);
+                    prevRow[k - 1] = currRow[k - 1]; // overrite values that won't be needed
+                }
+                currRow.Add(1);
+
+                // Terminate to copy prevRow <-- currRow
+                // Note that it is easier to read this code
+                // if I just copy all values in a loop
+                // However I overrite all values I can during
+                // previous loop so now I only need to copy last values
+                prevRow[k-1] = currRow[k-1];
+                prevRow.Add(1);
+
+                // Now that prevRow has currentRow values
+                // Clean currRow for next level
+                currRow.Clear();
+            }
+
+            // B(n) = sum_{k = 0}^{n} S(n,k)
+            BigInteger b = 0;
+            foreach (var s in prevRow)
+                b += s;
+
+            return b;
+        }
+
+    }
+
+
+}
---
+++ BellNumbers/README.md
@@ -0,0 +1,43 @@
+﻿﻿## Bell Numbers
+
+Use DP to solve catalan independentist numbers.
+Implements the recursive version for comparison.
+
+__Complexity__
+* DP version: polinomial.
+* Recursive: exponential.
+
+__References__
+
+[[1]](https://en.wikipedia.org/wiki/Catalan_number) Catalan number, Wikipedia.
+
+## Notes
+
+![Alt text](/CatalanNumbers/Recursive_approach.png?raw=true "Recursive approach")
+
+![Alt text](/CatalanNumbers/DP_approach.png?raw=true "DP approach")
+
+
+## Code Snippet
+
+```c#
+        static BigInteger CatalanDynamicProgramming(int n)
+        {
+            BigInteger[] c = new BigInteger[n + 1];
+
+            c[0] = 1;
+
+            for (int k = 1; k <= n; k++)
+            {
+                for (int i = 0; i <= k - 1; i++)
+                {
+                    c[k] += c[i] * c[k - 1 - i];
+                }
+            }
+            return c[n];
+
+        }
+```
+
+## Some Thoughts
+Need to use BigInteger data types since catalan numbers grow large pretty fast.
