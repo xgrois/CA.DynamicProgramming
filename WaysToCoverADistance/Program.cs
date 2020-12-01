c56bc485fa231dc1824dbfc27a5c8e426bb6c364
xgrois <67983850+xgrois@users.noreply.github.com>
2020-12-01 13:35:47 +0100

    Add WaysToCoverADistance problem

---
+++ WaysToCoverADistance/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WaysToCoverADistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("::: Counting the number of ways to cover a distance :::");
+            Console.WriteLine("::: (Using steps of 1, 2 and 3)                     :::\n\r");
+
+            int n = 30;
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int r1 = CountOfRecursive(n);
+            Console.WriteLine($"CountOfRecursive({n}) = {r1}. Elapsed time (ms): {watch.ElapsedMilliseconds}");
+            watch.Stop();
+
+            watch.Restart();
+            int r2 = CountOfDP(n);
+            watch.Stop();
+            Console.WriteLine($"CountOfDP({n})        = {r2}. Elapsed time (ms): {watch.ElapsedMilliseconds}");
+
+        }
+
+        static int CountOfRecursive(int n)
+        {
+
+            if (n == 0 || n == 1) return 1;
+            if (n == 2) return 2;
+
+            return (CountOfRecursive(n - 1) + CountOfRecursive(n - 2) + CountOfRecursive(n - 3));
+        }
+
+        static int CountOfDP(int n)
+        {
+            int result = 0;
+
+            if (n == 0 || n == 1) result = 1;
+            if (n == 2) result = 2;
+
+            int f0 = 1, f1 = 1, f2 = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                result = f0 + f1 + f2;
+                f0 = f1;
+                f1 = f2;
+                f2 = result;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ WaysToCoverADistance/README.md
@@ -0,0 +1,28 @@
+﻿## Count the number of ways to cover a distance
+
+Use DP to solve this problem: 
+
+Given a distance (0+ integer), count total number of ways to cover the distance using steps of 1, 2 and 3 sizes.
+
+__Complexity__
+* DP version (not memo, not tabular): linear O(n). You do not need a table.
+* Recursive: exponential.
+
+__References__
+
+[[1]](https://www.geeksforgeeks.org/count-number-of-ways-to-cover-a-distance/) Count number of ways to cover a distance, GeeksforGeeks.
+
+## Notes
+I came up with the DP solution directly.
+I spent some time thinking in the recursive way but no success. 
+However, I was confident that the recurrence relation can be derived directly.
+Thus, I made a spreadsheet for deriving it graphically and to illustrate the concept clearly.
+
+![Alt text](/WaysToCoverADistance/notes.JPG?raw=true "Notes")
+
+__Recurrence relation:__
+<img src="https://render.githubusercontent.com/render/math?math=F(n) = F(n-1) + F(n-2) + F(n-3)">
+
+## Output example
+
+![Alt text](/WaysToCoverADistance/output.JPG?raw=true "Output")
