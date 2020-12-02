fda1fb0658ad0a396b8ab72bfc583681447c4c83
xgrois <67983850+xgrois@users.noreply.github.com>
2020-12-02 19:23:20 +0100

    Add LongestIncreasingPathInMatrix

---
+++ LongestIncreasingPathInMatrix/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace LongestIncreasingPathInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("::: Longest increasing path in a matrix :::\n\r");
+
+            //int[][] matrix = null;
+            //int[][] matrix = new int[0][];
+            //int[][] matrix = new int[2][];
+
+            //int[][] matrix = new int[1][] { new int[] { } };
+
+            //int[][] matrix = new int[1][] { new int[] { 3 } };
+
+            //int[][] matrix = new int[1][] { new int[] { 3, 2, 1, 3, 4, 5 } };
+
+            //int[][] matrix = new int[3][] { new int[] { 3 }, new int[] { 5 }, new int[] { 1 } };
+
+            //int[][] matrix = new int[][] { new int[] { 9, 9, 4 }, new int[] { 6, 6, 8 }, new int[] { 2, 1, 1 } };
+            //int[][] matrix = new int[][] { new int[] { 3, 4, 5 }, new int[] { 3, 2, 6 }, new int[] { 2, 2, 1 } };
+
+            //int[][] matrix = new int[][] {
+            //    new int[] { 12, 13, 14, 15 },
+            //    new int[] { 9, 9, 4, 3 },
+            //    new int[] { 6, 6, 8, 2 },
+            //    new int[] { 2, 1, 1, 1 } };
+
+            int[][] matrix = new int[][] {
+                new int[] { 12, 13, 14, 15 },
+                new int[] { 9, 10, 9, 3 },
+                new int[] { 6, 7, 8, 2 },
+                new int[] { 2, 1, 1, 1 } };
+
+            Console.WriteLine($"Result = {Longest(matrix)}");
+
+        }
+
+        private static bool IsAtLeast1x1Matrix(int[][] matrix)
+        {
+            if (matrix != null)
+                if (matrix.Length >= 1)
+                    if (matrix[0] != null)
+                        if (matrix[0].Length >= 1)
+                            return true;
+            return false;
+        }
+
+        private static int Longest(int[][] matrix)
+        {
+
+            if (!IsAtLeast1x1Matrix(matrix)) return 0;
+
+            // Matrix is 1x1 or greater
+
+            int max = 1;
+            int N = matrix.Length;
+            int M = matrix[0].Length;
+
+            int[][] dp = new int[N][];
+            for (int r = 0; r < N; r++)
+            {
+                dp[r] = new int[M];
+
+                for (int c = 0; c < M; c++)
+                {
+                    dp[r][c] = 1;
+                }
+            }
+
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    ProcessCell(i, j, matrix, dp, 1, ref max);
+                }
+            }
+
+            PrintMatrix(dp);
+
+            return max;
+        }
+
+        private static void ProcessCell(int i, int j, int[][] matrix, int[][] dp, int count, ref int max)
+        {
+            int N = matrix.Length;
+            int M = matrix[0].Length;
+
+            // Process Left
+            if (isSafe(i, j - 1, N, M))
+            {
+                // Process only if we improve the results
+                if ((matrix[i][j - 1] < matrix[i][j]) && (count + 1 > dp[i][j - 1]))
+                {
+                    dp[i][j - 1] = count + 1;
+                    if (count + 1 > max) max = count + 1;
+                    ProcessCell(i, j - 1, matrix, dp, count + 1, ref max);
+                }
+
+            }
+            // Process Up
+            if (isSafe(i - 1, j, N, M))
+            {
+                // Process only if we improve the results
+                if ((matrix[i - 1][j] < matrix[i][j]) && (count + 1 > dp[i - 1][j]))
+                {
+                    dp[i - 1][j] = count + 1;
+                    if (count + 1 > max) max = count + 1;
+                    ProcessCell(i - 1, j, matrix, dp, count + 1, ref max);
+                }
+
+            }
+            // Process Right
+            if (isSafe(i, j + 1, N, M))
+            {
+                // Process only if we improve the results
+                if ((matrix[i][j + 1] < matrix[i][j]) && (count + 1 > dp[i][j + 1]))
+                {
+                    dp[i][j + 1] = count + 1;
+                    if (count + 1 > max) max = count + 1;
+                    ProcessCell(i, j + 1, matrix, dp, count + 1, ref max);
+                }
+
+            }
+            // Process Down
+            if (isSafe(i + 1, j, N, M))
+            {
+                // Process only if we improve the results
+                if ((matrix[i + 1][j] < matrix[i][j]) && (count + 1 > dp[i + 1][j]))
+                {
+                    dp[i + 1][j] = count + 1;
+                    if (count + 1 > max) max = count + 1;
+                    ProcessCell(i + 1, j, matrix, dp, count + 1, ref max);
+                }
+
+            }
+        }
+
+        private static bool isSafe(int i, int j, int N, int M)
+        {
+            // 0 <= i < N
+            // 0 <= j < M
+            return (0 <= i) && (i < N) && (0 <= j) && (j < M);
+        }
+
+        static void PrintMatrix(int[][] matrix)
+        {
+            if (!IsAtLeast1x1Matrix(matrix)) { Console.WriteLine("[]"); return; }
+
+            int N = matrix.Length;
+            int M = matrix[0].Length;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write($"{matrix[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ LongestIncreasingPathInMatrix/README.md
@@ -0,0 +1,68 @@
+﻿## Matrix longest increasing path problem
+
+__Problem statement__
+
+Given an integer matrix, find the length of the longest increasing path.
+
+From each cell, you can either move to four directions: left, right, up or down.
+You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
+
+__Difficulty in literature__
+
+This problem is considered Hard in literature.
+
+__Complexity__
+
+* DP version (tabular): less than exponential
+
+__References__
+
+[[1]](https://leetcode.com/problems/longest-increasing-path-in-a-matrix/) Find the longest path in a matrix, LeetCode.
+
+## Notes
+
+In this problem any cell may generate a tree of possible paths.
+Processing all in that sense could be considered the exponential-recursive solution.
+
+The idea is to avoid re-processing as possible (dynamic programming).
+My solution will only re-process a path if we are increasing the lenght of it.
+
+The idea is illustrated here:
+
+![Alt text](/LongestIncreasingPathInMatrix/example01.JPG?raw=true "Example01")
+
+![Alt text](/LongestIncreasingPathInMatrix/example02.JPG?raw=true "Example02")
+
+and so on...
+
+This idea is reflected in code as follows:
+
+```c#
+        private static void ProcessCell(int i, int j, int[][] matrix, int[][] dp, int count, ref int max)
+        {
+            int N = matrix.Length;
+            int M = matrix[0].Length;
+
+            // Process Left
+            if (isSafe(i, j - 1, N, M))
+            {
+                // Process only if we improve the results
+                if ((matrix[i][j - 1] < matrix[i][j]) && (count + 1 > dp[i][j - 1]))
+                {
+                    dp[i][j - 1] = count + 1;
+                    if (count + 1 > max) max = count + 1;
+                    ProcessCell(i, j - 1, matrix, dp, count + 1, ref max);
+                }
+
+            }
+            // Process Up
+            ...
+            // Process Right
+            ...
+            // Process Down
+
+´´´
+
+## Output example
+
+![Alt text](/LongestIncreasingPathInMatrix/output.JPG?raw=true "Output")
